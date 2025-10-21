3585a3b50ac8c640f3b382e72e67d19e74e8edd7
Alejandro <betancorta666@gmail.com>
2025-10-21 12:14:43 +0100

    .

--- paginaPeliculas/catalogo.php
+++ paginaPeliculas/catalogo.php
@@ -4,10 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Catalogo Películas</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h2></h2>
-    <img src="" alt="">
+    <form action="" method="post">
+        <select name="Genero">
+            <option value="Genero" selected>Genero</option>
+            <option value="Fantasía">Fantasía</option>
+            <option value="Ficción">Ficción</option>
+            <option value="Acción">Acción</option>
+            <option value="Drama">Drama</option>
+            <option value="Romance">Romance</option>
+        </select>
+        <input type="submit">
+    </form>
+    
 </body>
 </html>
 
@@ -18,46 +29,68 @@ $peliculas = [
         "director" => "Peter Jackson",
         "anio" => 2001,
         "genero" => "Fantasía",
-        "poster" => "imagenes/lotr1.jpg"
+        "poster" => "https://es.web.img3.acsta.net/medias/nmedia/18/89/67/45/20061512.jpg"
     ],
     [
         "titulo" => "Inception",
         "director" => "Christopher Nolan",
         "anio" => 2010,
         "genero" => "Ciencia Ficción",
-        "poster" => "imagenes/inception.jpg"
+        "poster" => "https://m.media-amazon.com/images/I/912AErFSBHL._AC_UF1000,1000_QL80_.jpg"
     ],
     [
         "titulo" => "Gladiator",
         "director" => "Ridley Scott",
         "anio" => 2000,
         "genero" => "Acción / Drama",
-        "poster" => "imagenes/gladiator.jpg"
+        "poster" => "https://es.web.img3.acsta.net/medias/nmedia/18/70/92/02/20149073.jpg"
     ],
     [
         "titulo" => "Interstellar",
         "director" => "Christopher Nolan",
         "anio" => 2014,
         "genero" => "Ciencia Ficción",
-        "poster" => "imagenes/interstellar.jpg"
+        "poster" => "https://m.media-amazon.com/images/M/MV5BYzdjMDAxZGItMjI2My00ODA1LTlkNzItOWFjMDU5ZDJlYWY3XkEyXkFqcGc@._V1_.jpg"
     ],
     [
         "titulo" => "Titanic",
         "director" => "James Cameron",
         "anio" => 1997,
         "genero" => "Romance / Drama",
-        "poster" => "imagenes/titanic.jpg"
+        "poster" => "https://pics.filmaffinity.com/Titanic-321994924-large.jpg"
     ],
     [
         "titulo" => "Avatar",
         "director" => "James Cameron",
         "anio" => 2009,
         "genero" => "Ciencia Ficción / Aventura",
-        "poster" => "imagenes/avatar.jpg"
+        "poster" => "https://pics.filmaffinity.com/Avatar-993297010-large.jpg"
     ]
 ];
+    echo "<prem>";
 
-for ($i=0; $i <= strlen($peliculas()); $i++) { 
-    echo $peliculas[i];
+    //FUNCTION
+for ($x=0; $x < count($peliculas); $x++) { 
+   if(strpos($peliculas[i]['genero'], $_POST(Genero)) != ""){
+        echo "<h2> " . $peliculas[$x]['titulo'] . "</h2> <br>" .
+            "<p> " . $peliculas[$x]['director'] . "</p> <br>" . 
+            "<p> " . $peliculas[$x]['anio'] . "</p> <br>". 
+            "<p> " . $peliculas[$x]['genero'] . "</p> <br>
+            <img src= '".$peliculas[$x]['poster']."'></img><br> <br>";
+   }
+   else{
+    return false;
+   }
+}
+
+
+
+//IF FALSE :
+for ($i=0; $i < count($peliculas); $i++) { 
+    echo "<h2> " . $peliculas[$i]['titulo'] . "</h2> <br>" .
+    "<p> " . $peliculas[$i]['director'] . "</p> <br>" . 
+    "<p> " . $peliculas[$i]['anio'] . "</p> <br>". 
+    "<p> " . $peliculas[$i]['genero'] . "</p> <br>
+    <img src= '".$peliculas[$i]['poster']."'></img><br> <br>";
 }
 ?>
